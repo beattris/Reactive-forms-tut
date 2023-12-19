8f39d9dc7d8fe775bd08616aa857e3ddfc74e703
beattris <beatriceebirim@gmail.com>
2023-12-19 07:36:30 +0100

    created and used a custom validator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,15 +9,19 @@ import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 export class AppComponent implements OnInit {
   genders = ['male', 'female'];
   signupForm: FormGroup;
+  forbiddenUsernames = ['Chris', 'Anna'];
 
   ngOnInit() {
     this.signupForm = new FormGroup({
-      'userData': new FormGroup({
-        'username': new FormControl(null, Validators.required),
-        'email': new FormControl(null, [Validators.required, Validators.email]),
+      userData: new FormGroup({
+        username: new FormControl(null, [
+          Validators.required,
+          this.forbiddenNames.bind(this)
+        ]),
+        email: new FormControl(null, [Validators.required, Validators.email]),
       }),
-      'gender': new FormControl('female'),
-      'hobbies': new FormArray([])
+      gender: new FormControl('female'),
+      hobbies: new FormArray([]),
     });
   }
 
@@ -32,5 +36,14 @@ export class AppComponent implements OnInit {
   onAddHobby() {
     const control = new FormControl(null, Validators.required);
     (<FormArray>this.signupForm.get('hobbies')).push(control);
+  }
+
+  // A custom validator to check forbidden usernames
+  //  the "{ [s: string]: boolean }" is used to assign a key value pair of type string to inform Angular
+  forbiddenNames(control: FormControl): { [s: string]: boolean } {
+    if (this.forbiddenUsernames.indexOf(control.value) !== -1) {
+      return { nameIsForbidden: true };
+    }
+    return null;
   }
 }
